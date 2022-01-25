a30105aee7b6e88edbdd12f901cd3dd05b048523
Jianglexing <1721900707@qq.com>
2022-01-25 16:39:20 +0800

    init

---
+++ src/main/java/com/sqlpy/tdds/TddsApplication.java
@@ -0,0 +1,13 @@
+package com.sqlpy.tdds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TddsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sqlpy/tdds/TddsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sqlpy.tdds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
